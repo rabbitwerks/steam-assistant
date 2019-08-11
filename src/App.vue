42eb6065004da5823a8693f343c63950c12b5038
SeanBecker <rabbitwerks.js@gmail.com>
2019-08-10 21:00:50 -0500

    init cleanup, styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="fxbx fd-c">
+    <div class="header fxbx a-ctr">
+      <img src="./assets/img/Steam_icon_logo.svg" alt="steam logo">
+      <div class="header--inner f1 fxbx spc-bt a-ctr">
+        <h2 class="text-heading">STEAM ASSISTANT</h2>
+        <button class="btn">
+          Assist!
+        </button>
+      </div>
+    </div>
+    <div class="content f1"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style>
+@import url('./assets/css/flexbox-util.css');
+@import url('./assets/css/main.css');
+
 #app {
+  min-height: 100vh;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  background-color: var(--bg-blue);
   color: #2c3e50;
-  margin-top: 60px;
+}
+.header {
+  height: 6rem;
+  padding-right: 1.5rem;
+  background: var(--nav-grey);
+}
+.header img {
+  height: 4rem;
+  margin: 0 1.5rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import { plugin } from 'vue-function-api';
 import App from './App.vue';
 import store from './store';
+
+Vue.use(plugin);
 
 Vue.config.productionTip = false;
 
